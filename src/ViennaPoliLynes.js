cde8a6cda73303cb44f12ab10dc35a87c713073e
bleavjac <b.levajac@gmail.com>
2017-09-18 14:32:52 +0200

    izradene linije (metro linije) koje povezivaju tocke(stanice) naliniji

--- src/ViennaPoliLynes.js
+++ src/ViennaPoliLynes.js
@@ -6,41 +6,16 @@ import geojsonDataByLines from './vienna_metro_lines_byLines.json';
 
 //main component
 class ViennaPoliLynes extends Component {
-  constructor() {
-    super();
-    this.state = {
-        geojsonDataByLines: geojsonDataByLines
-
-    };
-  }
-
-  polylineLineMaker() {
-    const geojsonDataByLines = this.state.geojsonDataByLines;
-
-    const testMe = geojsonDataByLines.features.map((cord) => {
-        return cord.geometry.coordinates;
-    });
-
-    return testMe;
-  }
-
-  polylineLineColor() {
-    const geojsonDataByLines = this.state.geojsonDataByLines;
-
-    const testMe = geojsonDataByLines.features.map((cord) => {
-        console.log(cord.properties.color)
-        return cord.properties.color;
-    });
-
-    return testMe;
-  }
-
   render() {
-
     return (
-        <Polyline positions={this.polylineLineMaker()} color={this.polylineLineColor()}>
-
-        </Polyline>
+      <div>
+        {geojsonDataByLines.features.map((feature) => (
+            <Polyline
+              positions={feature.geometry.coordinates}
+              color={feature.properties.color}
+              />
+          ))}
+      </div>
     );
   }
 }
